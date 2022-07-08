f5c71605e243e7c38c5f648fdd66c88de8fff701
Unmesh Chougule <chougule.unmesh@gmail.com>
2022-07-09 01:23:08 +0530

    Merge sort intermediate commit.

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ You can clone the code and try running the main methods in corresponding class t
 
 ### Sorting Algorithms
 1. Selection Sort
-
 2. Bubble Sort
-
 3. Insertion Sort
+4. Merge Sort
--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -10,7 +10,7 @@ import util.ArrayElementsPrinter;
 public class MergeSort {
 
     public static void main(String[] args) {
-        int[] arr = {64, 25, 12, 22, 11};
+        int[] arr = {64, 25, 12, 22, 11, 50};
         System.out.print("Array Before Sorting :");
         ArrayElementsPrinter.print(arr);
 
@@ -23,6 +23,8 @@ public class MergeSort {
     /**
      * Sorts the given array using merge sort algorithm
      * @param arr given int array
+     * @param left index from where first sub array will start from original array
+     * @param right index where second sub array will end
      */
     public static void sort(int[] arr, int left, int right) {
         if (left >= right) return;
@@ -35,9 +37,53 @@ public class MergeSort {
     }
 
     /**
-     * Merges two sorted arrays
+     * Merges two sorted arrays to get one sorted array
+     * @param arr original array
+     * @param start1 starting index of first sorted sub array
+     * @param end1 mid index (end index of first sorted sub array)
+     * @param end2 end index for second sorted sub array
      */
-    public static void merge(int[] arr, int left, int mid, int right) {
+    public static void merge(int[] arr, int start1, int end1, int end2) {
+        int start2 = end1+1;
+        int length2 = (end2-start2) + 1;
+        int length1 = (end1-start1) + 1;
+        int[] tempArr = new int[length1 + length2]; // to hold two merged array
 
+        int countToFillTempArray = 0;
+        int counterLeft = start1;
+        int counterRight = start2;
+
+        //in our case we are calculating middle index such that the second sub array will be always smaller or equal to first
+        //if this case is not known, we will have to check which array is smaller and iterate as per it's length
+        while (counterLeft<=end1 && counterRight<=end2) {
+            //compare both element and increment index when we add the element to temp array
+            if (arr[counterLeft] >= arr[counterRight]) {
+                tempArr[countToFillTempArray] = arr[counterRight];
+                counterRight++;
+            } else {
+                tempArr[countToFillTempArray] = arr[counterLeft];
+                counterLeft++;
+            }
+            countToFillTempArray++;
+        }
+
+        //if first array still has any remaining array then add them to temp array
+        while (counterLeft <= end1) {
+            tempArr[countToFillTempArray] = counterLeft;
+            counterLeft++;
+            countToFillTempArray++;
+        }
+
+        //if second array still has any remaining array then add them to temp array
+        while (counterRight <= end2) {
+            tempArr[countToFillTempArray] = counterRight;
+            counterRight++;
+            countToFillTempArray++;
+        }
+
+        //fill two merged arrays into main array
+        for (int i=0; i<tempArr.length; i++) {
+            arr[start1] = tempArr[i];
+        }
     }
 }
